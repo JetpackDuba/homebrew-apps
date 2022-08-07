7c1d62f3fe33b737071a7fba91dc868c370e234f
Abdelilah El Aissaoui <aeab13@gmail.com>
2022-08-07 18:21:25 +0200

    Updated file URL

--- Gitnuro.rb
+++ Gitnuro.rb
@@ -1,7 +1,7 @@
 class Gitnuro < Formula
   desc "Gitnuro Git Client"
   homepage "gitnuro.jetpackduba.com"
-  url "https://github.com/JetpackDuba/Gitnuro/releases/download/untagged-03b07424310a887828fe/Gitnuro-macos-1.0.1.tar.gz"
+  url "https://github.com/JetpackDuba/homebrew-apps/releases/download/1.0.1/Gitnuro-macos-1.0.1.tar.gz"
   sha256 "b199288d5051773fcd003a13d5d1c9882248a228160eb734e3677a9f1b6a4841"
   license "GNU General Public License v3.0"
   
