af776bbc1e9134bd7207d713678ebb105a858c70
Abdelilah El Aissaoui <aeab13@gmail.com>
2022-08-07 18:37:17 +0200

    Changed formulae to cask

---
+++ Casks/Gitnuro.rb
@@ -0,0 +1,11 @@
+cask "Gitnuro" do
+  version "1.0.1"
+  sha256 "83eacfa269a09a94464d100b9c2e9e73ffbcc379833ebaecbe5b817fb4450e28"
+
+  name "Gitnuro"
+  desc "FOSS multiplatform git client"
+  homepage "https://gitnuro.jetpackduba.com"
+  url "https://github.com/JetpackDuba/homebrew-apps/releases/download/1.0.1/Gitnuro-macos-1.0.1.zip"
+  
+  app "Gitnuro.app"
+end
