3766e08481a39a1155d138a57b976ca5496a5381
Abdelilah El Aissaoui <aeab13@gmail.com>
2022-10-30 00:36:25 +0200

    Updated macOS release

--- Casks/gitnuro.rb
+++ Casks/gitnuro.rb
@@ -1,11 +1,11 @@
 cask "gitnuro" do
-  version "1.0.1"
-  sha256 "7ee70f698f32c17aafec727cd0eb26f2c8526c3a2b0538ef966230922a482130"
+  version "1.1.0"
+  sha256 "804e1ba8d7e7180fde572d3e9848ab332d0bc28cf38bbcd6484b637c935544df"
 
   name "Gitnuro"
   desc "FOSS multiplatform git client"
   homepage "https://gitnuro.jetpackduba.com"
-  url "https://github.com/JetpackDuba/Gitnuro/releases/download/v1.0.1/Gitnuro-macos-1.0.1.zip"
+  url "https://github.com/JetpackDuba/Gitnuro/releases/download/v1.1.0/Gitnuro-macos-1.1.0.zip"
   
   app "Gitnuro.app"
 end
