622cc844c087b922cb6a511abd9c82e76623a8d5
Abdelilah El Aissaoui <aeab13@gmail.com>
2024-10-06 02:40:48 +0200

    Prepare v1.4.0

--- Casks/gitnuro.rb
+++ Casks/gitnuro.rb
@@ -1,11 +1,11 @@
 cask "gitnuro" do
-  version "1.3.1"
-  sha256 "cb62e5921c058844d8b0a2900ae3316f86d8baacbb00d1b7c89d5e4dc40185d1"
+  version "1.4.0"
+  sha256 "511a95fa79ac3cdfec4250774962611f7ade97c1db0f9adb150b6abbe48e2196"
 
   name "Gitnuro"
   desc "FOSS multiplatform git client"
-  homepage "https://gitnuro.jetpackduba.com"
-  url "https://github.com/JetpackDuba/Gitnuro/releases/download/v1.3.1/Gitnuro-macos-1.3.1.zip"
+  homepage "https://gitnuro.com"
+  url "https://github.com/JetpackDuba/Gitnuro/releases/download/v1.4.0/Gitnuro_macos_1.4.0.zip"
   
   app "Gitnuro.app"
 end
